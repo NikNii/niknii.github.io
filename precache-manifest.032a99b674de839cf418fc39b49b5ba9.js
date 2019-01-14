40210cd69282f30b51e44a6878f697e50d1af144
Niki <niittymaki.niki@gmail.com>
2019-01-14 04:22:07 +0200

    Updates

---
+++ precache-manifest.032a99b674de839cf418fc39b49b5ba9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc06cb6a5eeb7a2c0bad",
+    "url": "/weather-web-app/static/js/runtime~main.bc06cb6a.js"
+  },
+  {
+    "revision": "2ab03c442d029b2f8573",
+    "url": "/weather-web-app/static/js/main.2ab03c44.chunk.js"
+  },
+  {
+    "revision": "ebcea85a64f88ceda3ed",
+    "url": "/weather-web-app/static/js/1.ebcea85a.chunk.js"
+  },
+  {
+    "revision": "2ab03c442d029b2f8573",
+    "url": "/weather-web-app/static/css/main.005da498.chunk.css"
+  },
+  {
+    "revision": "a3381868f37769a8668e0447af3f5da3",
+    "url": "/weather-web-app/index.html"
+  }
+];
